42e8e2e232f730cc81c4b92425e7cf23eabe74ed
repoGitSantini <santinimaximiliano1@gmail.com>
2024-08-04 21:23:14 -0300

    Add files via upload

---
+++ Formulario_ej_clase_1/Ejercicio_0.js
@@ -0,0 +1,7 @@
+  document.getElementById("estilo1").addEventListener("click", function() {
+    document.getElementById("estilosEj0").setAttribute("href", "Ejercicio_0.css");
+  });
+
+  document.getElementById("altoContraste").addEventListener("click", function() {
+    document.getElementById("estilosEj0").setAttribute("href", "Ejercicio_0-contraste.css");
+  });
